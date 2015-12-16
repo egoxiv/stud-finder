e1468f1d354092c0f71d73ef2b4c7433f7d64ec3
Erik Gomez <egoxiv@gmail.com>
2015-12-15 21:37:13 -0800

    Added jquery file to html

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,32 +1,34 @@
 $(function(){
 
-  // function playButton(){
-  //   $(".play").on("click", function(){
-  //     var $gallery = $(".gallery");
-  //     var $video = $(".video");
-  //     var $returnButton = $(".return-button");
+  function playButton(){
+    $(".play").on("click", function(){
+      var $gallery = $(".gallery");
+      var $video = $(".video");
+      var $returnButton = $(".return-button");
 
-  //     $gallery.slideUp("fast");
-  //     $video.removeClass("hide");
+      $gallery.slideUp("fast");
+      $video.removeClass("hide");
 
-  //     $returnButton.on("click", function(){
-  //       $gallery.slideDown("fast");
-  //       $video.addClass("hide");
-  //     });
-  //   })
-  // }
+      $returnButton.on("click", function(){
+        $gallery.slideDown("fast");
+        $video.addClass("hide");
+      });
+    })
+  }
 
-  // playButton();
+  playButton();
 
-function handlebars(){
-  var template = Handlebars.compile($('templates/gallery.html').html());
-  console.log(template);
-  //var temp = template();
-  //console.log(temp);
-  $("#handlebarTemplate").append(temp);
-  //console.log(articleData);
-}
-handlebars();
+
+
+  function galleryTemplate() {
+    $.get("templates/gallery.html", function(data){
+      console.log(data);
+      var compileTemplate = Handlebars.compile(data);
+      var html = compileTemplate;
+      $(".template").append(html);
+    })
+  }
+  galleryTemplate();
 
 
 
