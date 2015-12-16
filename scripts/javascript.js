88f501af3f9e25d9ea7aa2fce9550baa28cc3fc9
Erik Gomez <egoxiv@gmail.com>
2015-12-15 17:51:12 -0800

    Added jquery file to test.html

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,25 +1,33 @@
 $(function(){
 
-  function playButton(){
-    $(".play").on("click", function(){
-      var $gallery = $(".gallery");
-      var $video = $(".video");
-      var $returnButton = $(".return-button");
+  // function playButton(){
+  //   $(".play").on("click", function(){
+  //     var $gallery = $(".gallery");
+  //     var $video = $(".video");
+  //     var $returnButton = $(".return-button");
 
-      $gallery.slideUp("fast");
-      $video.removeClass("hide");
+  //     $gallery.slideUp("fast");
+  //     $video.removeClass("hide");
 
-      $returnButton.on("click", function(){
-        $gallery.slideDown("fast");
-        $video.addClass("hide");
-      });
-    })
-  }
+  //     $returnButton.on("click", function(){
+  //       $gallery.slideDown("fast");
+  //       $video.addClass("hide");
+  //     });
+  //   })
+  // }
 
-  playButton();
+  // playButton();
+
+function handlebars(){
+  var template = Handlebars.compile($('templates/gallery.html').html());
+  console.log(template);
+  //var temp = template();
+  //console.log(temp);
+  $("#handlebarTemplate").append(temp);
+  //console.log(articleData);
+}
+handlebars();
+
+
+
 })
-//change .gallery to display: none;
-//button back to gallery inside .video div
-//keep div set to display none
-//remove display: none from div
-//
